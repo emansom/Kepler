e055abe292ab2594c48535a9a5c8337bd34eabc0
Quackster <quacksterr.15@gmail.com>
2018-01-10 14:56:54 +1000

    Add more packets

--- src/communication/incoming/InitCryptoMessageEvent.h
+++ src/communication/incoming/InitCryptoMessageEvent.h
@@ -2,7 +2,7 @@
 #include "communication/messages/incoming_message.h"
 
 void message_initcrypto(request message) {
-    char *response = "DUH\1";
+    char *response = "DUIH\1";
     dyad_write(message.player->stream, response, strlen(response));
 
 }
