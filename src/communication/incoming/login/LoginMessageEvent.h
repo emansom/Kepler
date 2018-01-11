5873d2d4719132f7035a4da72471c3a7ec6000a3
Quackster <quacksterr.15@gmail.com>
2018-01-11 22:08:04 +1000

    Debug login information

--- src/communication/incoming/login/LoginMessageEvent.h
+++ src/communication/incoming/login/LoginMessageEvent.h
@@ -10,6 +10,7 @@ void message_login(player *player, incoming_message *message) {
     char *username = im_read_str(message);
     char *password = im_read_str(message);
 
+    printf("Username: %s and password: %s.\n", username, password);
     player_data *player_data = query_player_login(username, password);
 
     if (player_data == NULL) {
@@ -17,7 +18,7 @@ void message_login(player *player, incoming_message *message) {
         return;
     }
 
-    printf("Username: %s and password: %s\n", username, password);
+
 
     outgoing_message *om;
 
