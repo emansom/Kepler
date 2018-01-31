f65b57e68facd2b2b5735bfa06e120471a274719
Quackster <quacksterr.15@gmail.com>
2018-01-31 21:08:56 +1000

    Remove ignored files

--- src/database/db_connection.c
+++ src/database/db_connection.c
@@ -13,30 +13,26 @@ connection_details db_connection_settings() {
     return settings;
 }
 
-char* load_file(char const* path)
-{
+char* load_file(char const* path) {
+    
     char* buffer = 0;
     long length;
     FILE * f = fopen (path, "rb"); //was "rb"
 
-    if (f)
-    {
-      fseek (f, 0, SEEK_END);
-      length = ftell (f);
-      fseek (f, 0, SEEK_SET);
-      buffer = (char*)malloc ((length+1)*sizeof(char));
-      if (buffer)
-      {
-        fread (buffer, sizeof(char), length, f);
-      }
-      fclose (f);
-    }
-    buffer[length] = '\0';
-    // for (int i = 0; i < length; i++) {
-    //     printf("buffer[%d] == %c\n", i, buffer[i]);
-    // }
-    //printf("buffer = %s\n", buffer);
+    if (f) {
+        fseek (f, 0, SEEK_END);
+        length = ftell (f);
+        fseek (f, 0, SEEK_SET);
+        buffer = (char*)malloc ((length+1)*sizeof(char));
 
+        if (buffer) {
+            fread (buffer, sizeof(char), length, f);
+        }
+
+        fclose (f);
+    }
+
+    buffer[length] = '\0';
     return buffer;
 }
 
