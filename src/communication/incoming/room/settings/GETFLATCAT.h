95f5d1725fa20a0d6759c1faf0156d3c1b4a723e
Quackster <quacksterr.15@gmail.com>
2018-02-01 02:30:05 +1000

    Prevent null room

--- src/communication/incoming/room/settings/GETFLATCAT.h
+++ src/communication/incoming/room/settings/GETFLATCAT.h
@@ -5,6 +5,10 @@ void GETFLATCAT(player *player, incoming_message *message) {
     int room_id = im_read_vl64(message);
 	room *room = room_query_get_by_room_id(room_id);
 
+	if (room == NULL) {
+		return;
+	}
+
 	outgoing_message *om = om_create(222); // "C^"
 	
 	if (room != NULL) {
