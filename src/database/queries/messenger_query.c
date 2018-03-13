72afef477906280cc2320668dad6d43c9524c439
Alex <quacksterr.15@gmail.com>
2018-03-14 00:56:30 +1000

    Fix get friends query and return list of friends

--- src/database/queries/messenger_query.c
+++ src/database/queries/messenger_query.c
@@ -11,11 +11,13 @@
  * @return
  */
 List *messenger_query_get_friends(int user_id) {
+    List *friends;
+    list_new(&friends);
 
     sqlite3 *conn = db_create_connection();
     sqlite3_stmt *stmt;
 
-    int status = sqlite3_prepare(conn, "SELECT * FROM messenger_friends WHERE to_id = ? OR from_id = ?", -1, &stmt, 0);
+    int status = sqlite3_prepare(conn, "SELECT to_id, from_id FROM messenger_friends WHERE to_id = ? OR from_id = ?", -1, &stmt, 0);
 
     if (status == SQLITE_OK) {
         sqlite3_bind_int(stmt, 1, user_id);
@@ -31,8 +33,8 @@ List *messenger_query_get_friends(int user_id) {
             break;
         }
 
-        int to_id = sqlite3_column_int(stmt, 1);
-        int from_id = sqlite3_column_int(stmt, 2);
+        int to_id = sqlite3_column_int(stmt, 0);
+        int from_id = sqlite3_column_int(stmt, 1);
 
         int friend_id = -1;
 
@@ -42,11 +44,11 @@ List *messenger_query_get_friends(int user_id) {
             friend_id = from_id;
         }
 
-        printf("user id: %i\n", friend_id);
+        list_add(friends, (void*)&friend_id);
     }
 
     sqlite3_finalize(stmt);
     sqlite3_close(conn);
 
-    return NULL;
+    return friends;
 }
