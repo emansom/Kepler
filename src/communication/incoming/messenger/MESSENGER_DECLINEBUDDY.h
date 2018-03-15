99997c02e90d536db571f2c39118fdee918a1995
Alex <quacksterr.15@gmail.com>
2018-03-15 17:51:13 +1000

    Add option to reject all requests

--- src/communication/incoming/messenger/MESSENGER_DECLINEBUDDY.h
+++ src/communication/incoming/messenger/MESSENGER_DECLINEBUDDY.h
@@ -6,11 +6,7 @@
 #include "game/player/player.h"
 #include "database/queries/messenger_query.h"
 
-void MESSENGER_DECLINEBUDDY(player *session, incoming_message *message) {
-    im_read_vl64(message); // useless?
-
-    int friend_id = im_read_vl64(message);
-
+void remove_friend(player *session, int friend_id) {       
     messenger_query_delete_request(session->player_data->id, friend_id);
     messenger_query_delete_request(friend_id, session->player_data->id);
 
@@ -20,3 +16,21 @@ void MESSENGER_DECLINEBUDDY(player *session, incoming_message *message) {
         messenger_remove_request(friend->messenger, friend_id);
     }
 }
+
+void MESSENGER_DECLINEBUDDY(player *session, incoming_message *message) {
+    int decline_option = im_read_vl64(message);
+    
+    if (decline_option > 0) {
+        int request_id = im_read_vl64(message);
+        remove_friend(session, request_id);
+    } else {
+        for (int i = 0; i < list_size(session->messenger->requests); i++) {
+            messenger_entry *request;
+            list_get_at(session->messenger->requests, i, (void*)&request);
+            
+            int request_id = request->friend_id;
+            remove_friend(session, request_id);
+        }
+    }
+}
+
