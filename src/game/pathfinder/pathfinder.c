b8216b5a13cb1728625336ea2be6ddbf1ea4ceb3
Quackster <quacksterr.15@gmail.com>
2018-04-20 07:40:34 +1000

    Futher pathfinder fixes

--- src/game/pathfinder/pathfinder.c
+++ src/game/pathfinder/pathfinder.c
@@ -154,6 +154,26 @@ int is_valid_tile(room_user *room_user, coord from, coord to, bool is_final_move
         }
     }
 
+    if (from.x != room_instance->room_data->model_data->door_x
+        && from.y != room_instance->room_data->model_data->door_y) {
+
+        if (to_item != NULL) {
+            if (is_final_move) {
+                return item_is_walkable(to_item);
+            } else {
+
+                room_tile *current_tile = room_instance->room_map->map[from.x][from.y];
+
+                if (current_tile->highest_item != NULL) {
+                    return item_is_walkable(current_tile->highest_item);
+                } else {
+                    return to_item->definition->behaviour->can_stand_on_top;
+                }
+            }
+        }
+    }
+
+
     return true;
 }
 
