e02b86dd7a8e619d76f338b0cd6a70fbe31e60d0
Quackster <quacksterr.15@gmail.com>
2018-04-23 09:06:46 +1000

    Add purchase packages

--- src/communication/incoming/catalogue/GRPC.h
+++ src/communication/incoming/catalogue/GRPC.h
@@ -3,13 +3,17 @@
 
 #include "database/queries/item_query.h"
 
+#include "game/items/item.h"
 #include "game/inventory/inventory.h"
+
+void do_purchase(session *player, item_definition *def, char *extra_data, int special_sprite_id);
 
 void GRPC(session *player, incoming_message *message) {
     char *content = im_get_content(message);
 
     char *page = get_argument(content, "\r", 1);
     char *sale_code = get_argument(content, "\r", 3);
+
 
     if (page == NULL) {
         goto cleanup;
@@ -35,11 +39,6 @@ void GRPC(session *player, incoming_message *message) {
         goto cleanup;
     }
 
-    if (store_item->is_package) {
-        goto cleanup; // TODO: Purchase packages
-    }
-
-
     if (store_item->price > player->player_data->credits) {
         outgoing_message *om = om_create(68); // "AD"
         session_send(player, om);
@@ -47,25 +46,52 @@ void GRPC(session *player, incoming_message *message) {
 
         goto cleanup;
     }
-
-    char *custom_data = NULL;
-
     if (!store_item->is_package) {
         char *extra_data = get_argument(content, "\r", 4);
+        do_purchase(player, store_item->definition, extra_data, store_item->item_specialspriteid);
 
-        if (store_item->definition->behaviour->is_decoration) {
+        free(extra_data);
+    } else {
+        for (size_t i = 0; i < list_size(store_item->packages); i++) {
+            catalogue_package *package;
+            list_get_at(store_item->packages, i, (void *) &package);
+
+            do_purchase(player, package->definition, NULL, package->special_sprite_id);
+        }
+    }
+
+    player->player_data->credits -= store_item->price;
+    session_send_credits(player);
+
+    player_query_save_currency(player);
+
+    inventory *inv = (inventory *) player->inventory;
+    inventory_send(inv, "update", player);
+
+    cleanup:
+		free(content);
+		free(page);
+		free(sale_code);
+        
+}
+
+void do_purchase(session *player, item_definition *def, char *extra_data, int special_sprite_id) {
+    char *custom_data = NULL;
+
+    if (extra_data != NULL) {
+        if (def->behaviour->is_decoration) {
             if (is_numeric(extra_data)) {
                 custom_data = strdup(extra_data);
             }
         } else {
-            if (store_item->item_specialspriteid > 0) {
+            if (special_sprite_id > 0) {
                 char num[10];
-                sprintf(num, "%i", store_item->item_specialspriteid);
+                sprintf(num, "%i", special_sprite_id);
                 custom_data = strdup(num);
             }
         }
 
-        if (store_item->definition->behaviour->is_prize_trophy) {
+        if (def->behaviour->is_prize_trophy) {
             filter_vulnerable_characters(&extra_data, true);
 
             char *short_date = get_short_time_formatted();
@@ -82,25 +108,9 @@ void GRPC(session *player, incoming_message *message) {
             sb_cleanup(sb);
             free(short_date);
         }
-
-        free(extra_data);
     }
 
-    player->player_data->credits -= store_item->price;
-    session_send_credits(player);
-
-    int item_id = item_query_create(player->player_data->id, 0, store_item->definition->id, 0, 0, 0, 0, custom_data);
-    item *inventory_item = item_create(item_id, 0, store_item->definition->id, 0, 0, 0, NULL, 0, custom_data);
-
+    int item_id = item_query_create(player->player_data->id, 0, def->id, 0, 0, 0, 0, custom_data);
+    item *inventory_item = item_create(item_id, 0, def->id, 0, 0, 0, NULL, 0, custom_data);
     list_add(player->inventory->items, inventory_item);
-    player_query_save_currency(player);
-
-    inventory *inv = (inventory *) player->inventory;
-    inventory_send(inv, "update", player);
-
-    cleanup:
-		free(content);
-		free(page);
-		free(sale_code);
-        
 }
