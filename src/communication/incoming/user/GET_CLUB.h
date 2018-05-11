d9ec7df6b4a0f9bddc00257507a49f9aaf9afb2c
Quackster <quacksterr.15@gmail.com>
2018-05-11 11:14:23 +1000

    Neaten club files and move management to its own file

--- src/communication/incoming/user/GET_CLUB.h
+++ src/communication/incoming/user/GET_CLUB.h
@@ -1,7 +1,8 @@
 #include "communication/messages/incoming_message.h"
 #include "communication/messages/outgoing_message.h"
-#include "game/player/player.h"
+
+#include "game/club/club_manager.h"
 
 void GET_CLUB(session *player, incoming_message *message) {
-    player_refresh_club(player);
+    club_refresh(player);
 }
