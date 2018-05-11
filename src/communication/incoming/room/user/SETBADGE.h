5d029c55abd0966dd64cca9a3e36ec9b066b7e23
Quackster <quacksterr.15@gmail.com>
2018-05-12 01:05:16 +1000

    No reason to have update badge exposed to room_user.h

--- src/communication/incoming/room/user/SETBADGE.h
+++ src/communication/incoming/room/user/SETBADGE.h
@@ -4,6 +4,18 @@
 #include "game/player/player.h"
 #include "game/room/room_user.h"
 
+void update_badge(room_user *room_user) {
+    outgoing_message *badge_notify = om_create(228); // "Cd"
+
+    om_write_int(badge_notify, room_user->instance_id);
+
+    if (strlen(room_user->player->player_data->active_badge) > 0) {
+        om_write_str(badge_notify, room_user->player->player_data->active_badge);
+    }
+
+    room_send(room_user->room, badge_notify);
+}
+
 void SETBADGE(session *player, incoming_message *im) {
     char *new_badge = im_read_str(im); // TODO: check if user owns badge
     int show_badge = im_read_vl64(im);
@@ -16,7 +28,7 @@ void SETBADGE(session *player, incoming_message *im) {
 
     free(new_badge);
 
-    room_user_update_badge(player->room_user);
+    update_badge(player->room_user);
 
     // TODO: save to database
 }
