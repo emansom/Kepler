aa88556a42e4e0041594965f45e38ed15bc303a9
Quackster <quacksterr.15@gmail.com>
2018-05-12 00:39:05 +1000

    Revert edits done in commit a691c8012ad538df16fc72499a7bf4e6e0ec4825

--- src/communication/incoming/room/user/USER_START_TYPING.h
+++ src/communication/incoming/room/user/USER_START_TYPING.h
@@ -9,13 +9,11 @@
 #include "game/room/room_user.h"
 
 void USER_START_TYPING(session *player, incoming_message *im) {
-    if (player->room_user == NULL || player->room_user->is_typing) {
+    if (player->room_user->room == NULL || player->room_user->is_typing) {
         return;
     }
 
     player->room_user->is_typing = true;
-
-    room_user_reset_idle_timer(player->room_user);
 
     outgoing_message *om = om_create(361); // "Ei"
     om_write_int(om, player->room_user->instance_id);
