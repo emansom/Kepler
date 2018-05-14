650c5b8286d5c84db3061d6e74f749705dd9a0f0
Quackster <quacksterr.15@gmail.com>
2018-05-14 10:21:31 +1000

    Fuck you CodeDragon

--- src/communication/incoming/login/SSO.h
+++ src/communication/incoming/login/SSO.h
@@ -7,21 +7,21 @@
 void SSO(session *player, incoming_message *message) {
     char *ticket = im_read_str(message);
 
-    if (ticket != NULL) {
-        int player_id = player_query_sso(ticket);
+    if (ticket == NULL) {
+        return;
+    }
 
-        if (player_id == -1) {
-            player_send_localised_error(player, "Incorrect SSO ticket");
-            goto cleanup;
-        } else {
-            player_data *data = player_query_data(player_id);
-            player->player_data = data;
-        }
+    int player_id = player_query_sso(ticket);
+
+    if (player_id == -1) {
+        player_send_localised_error(player, "Incorrect SSO ticket");
+    } else {
+        player_data *data = player_query_data(player_id);
+        player->player_data = data;
 
         player_manager_destroy_session_by_id(player_id);
         player_login(player);
     }
 
-    cleanup:
     free(ticket);
 }
