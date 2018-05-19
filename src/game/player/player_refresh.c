850baf752536950fc6d33f6b978cd9bd7d1ac39c
Quackster <quacksterr.15@gmail.com>
2018-05-19 15:48:53 +1000

    Fix badge char* comparison

--- src/game/player/player_refresh.c
+++ src/game/player/player_refresh.c
@@ -148,7 +148,7 @@ void player_refresh_badges(entity *player) {
         char *badge = (char*)next;
         om_write_str(badge_list, badge);
 
-        if (badge == player->details->active_badge) {
+        if (strcmp(badge, player->details->active_badge) == 0) {
             badge_slot = slot_counter;
         }
 
