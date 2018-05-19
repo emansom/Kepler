62dbb71350a1e3b62e50208d557a669adc65acda
Quackster <quacksterr.15@gmail.com>
2018-05-20 00:57:54 +1000

    Add in-depth following

--- src/communication/incoming/messenger/FOLLOW_FRIEND.h
+++ src/communication/incoming/messenger/FOLLOW_FRIEND.h
@@ -11,21 +11,34 @@
 void FOLLOW_FRIEND(entity *player, incoming_message *message) {
     int target_id = im_read_vl64(message);
 
-    entity *player_friend = player_manager_find_by_id(target_id);
+    int error_id = -1;
 
-    if (player_friend != NULL && player_friend->room_user->room != NULL) {
-        bool is_public = list_size(player_friend->room_user->room->room_data->model_data->public_items) > 0;
+    if (messenger_is_friends(player->messenger, target_id)) {
+        entity *player_friend = player_manager_find_by_id(target_id);
 
-        outgoing_message *stalk_ok = om_create(286); // "D^"
-        //om_write_int(stalk_ok, 0); // TODO: figure out what this is
-        om_write_int(stalk_ok, is_public);
-        om_write_int(stalk_ok, player_friend->room_user->room_id);
-        player_send(player, stalk_ok);
-        om_cleanup(stalk_ok);
+        if (player_friend != NULL) {
+            if (player_friend->room_user->room != NULL) {
+                bool is_public = (player_friend->room_user->room->room_data->owner_id == 0);
+
+                outgoing_message *stalk_ok = om_create(286); // "D^"
+                om_write_int(stalk_ok, is_public);
+                om_write_int(stalk_ok, player_friend->room_user->room_id);
+                player_send(player, stalk_ok);
+                om_cleanup(stalk_ok);
+            } else {
+                error_id = 2; // User isn't in a room
+            }
+        } else {
+            error_id = 1; // Friend is offline
+        }
     } else {
+        error_id = 0; // You're not their friend
+    }
+
+    if (error_id != -1) {
         outgoing_message *stalk_err = om_create(349); // "E]"
-        om_write_int(stalk_err, 2); // probably error ID?
+        om_write_int(stalk_err, error_id); // probably error ID?
         player_send(player, stalk_err);
         om_cleanup(stalk_err);
-     }
+    }
 }
