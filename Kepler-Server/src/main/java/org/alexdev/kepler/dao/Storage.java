7ce1e289e0469ba55916ac90bfd45c84ec3b69f4
Ewout van Mansom <ewout@freedom.nl>
2018-05-21 22:49:56 +0200

    Set HikariCP pool size based on CPU thread count + 1 spindle

--- Kepler-Server/src/main/java/org/alexdev/kepler/dao/Storage.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/dao/Storage.java
@@ -29,7 +29,13 @@ public class Storage {
             config.setPassword(password);
             
             config.setPoolName("processing");
-            config.setMaximumPoolSize(5);
+
+            // No martinmine/Leon/other Habbotards, you don't know better.
+            // Read up on this first, before commenting dumb shit
+            // https://github.com/brettwooldridge/HikariCP/wiki/About-Pool-Sizing
+            // availableProcessors() already returns thread count on hyper-threaded processors
+            // Thus we don't need the * 2 described there
+            config.setMaximumPoolSize(Runtime.getRuntime().availableProcessors() + 1);
             config.setMinimumIdle(1);
             
             config.addDataSourceProperty("cachePrepStmts", "true");
