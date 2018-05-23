5b50b853747152f27ef17f2f52649cb675875a15
Quackster <quacksterr.15@gmail.com>
2018-05-23 23:10:18 +1000

    Show the last hand page where the bought item is

--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/catalogue/GRPC.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/catalogue/GRPC.java
@@ -55,7 +55,7 @@ public class GRPC implements MessageEvent {
         player.send(new CREDIT_BALANCE(player.getDetails()));
 
         PlayerDao.saveCurrency(player.getDetails());
-        player.getInventory().getView("new");
+        player.getInventory().getView("last");
     }
 
     private void purchase(Player player, ItemDefinition def, String extraData, int specialSpriteId) {
