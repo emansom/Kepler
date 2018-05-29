2d8422555a7ecfddcff23de54fd75cc45caa402c
Ewout van Mansom <ewout@freedom.nl>
2018-05-29 18:20:18 +0200

    Handle rotation and height calculation in :sit command

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/registered/SitCommand.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/registered/SitCommand.java
@@ -3,8 +3,11 @@ package org.alexdev.kepler.game.commands.registered;
 import org.alexdev.kepler.game.commands.Command;
 import org.alexdev.kepler.game.entity.Entity;
 import org.alexdev.kepler.game.entity.EntityType;
+import org.alexdev.kepler.game.item.Item;
 import org.alexdev.kepler.game.player.Player;
 import org.alexdev.kepler.game.room.enums.StatusType;
+import org.alexdev.kepler.game.room.mapping.RoomTile;
+import org.alexdev.kepler.util.StringUtil;
 
 public class SitCommand extends Command {
 
@@ -29,10 +32,42 @@ public class SitCommand extends Command {
             return;
         }
 
-        player.getRoomUser().removeStatus(StatusType.DANCE);
-        player.getRoomUser().setStatus(StatusType.SIT, "1.0");
-        player.getRoomUser().setNeedsUpdate(true);
+        RoomTile tile = player.getRoomUser().getTile();
+        double height = tile.getTileHeight();
 
+        if (height != player.getRoomUser().getPosition().getZ()) {
+            player.getRoomUser().getPosition().setZ(height);
+        }
+
+        Item item = null;
+
+        if (tile.getHighestItem() != null) {
+            item = tile.getHighestItem();
+        }
+
+        if (item != null) {
+            if (!item.getBehaviour().isCanSitOnTop()) {
+                return;
+            }
+
+            height = item.getDefinition().getTopHeight();
+            player.getRoomUser().getPosition().setRotation(item.getPosition().getRotation());
+            player.getRoomUser().setStatus(StatusType.SIT, StringUtil.format(item.getDefinition().getTopHeight()));
+        } else {
+            int rotation = player.getRoomUser().getPosition().getRotation();
+
+            if (rotation > 0 && rotation <= 7) {
+                if (rotation % 2 == 0) {
+                    rotation--;
+                }
+            }
+
+            player.getRoomUser().getPosition().setRotation(rotation);
+            player.getRoomUser().setStatus(StatusType.SIT, StringUtil.format(tile.getTileHeight()));
+        }
+
+        player.getRoomUser().removeStatus(StatusType.DANCE);
+        player.getRoomUser().setNeedsUpdate(true);
     }
 
     @Override
