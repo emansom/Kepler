059cfc34f3fed22a3e836d89026ee0ad2e736694
Ewout van Mansom <ewout@freedom.nl>
2018-05-30 09:15:22 +0200

    Fix replace on modelType in CREATEFLAT.java

--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/rooms/settings/CREATEFLAT.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/rooms/settings/CREATEFLAT.java
@@ -34,17 +34,17 @@ public class CREATEFLAT implements MessageEvent {
             return;
         }
 
-        String modelType = roomModel.replace("model_a", "");
+        String modelType = roomModel.replace("model_", "");
 
-        // TODO: handle fuse special room layouts correctly so it won't return for people that don't have rank >2
-//        if (!modelType.equals("a") &&
-//                !modelType.equals("b") &&
-//                !modelType.equals("c") &&
-//                !modelType.equals("d") &&
-//                !modelType.equals("f") &&
-//                !player.hasFuse("fuse_use_special_room_layouts")) {
-//            return; // Fuck off, scripter.
-//        }
+        // TODO: validate modelType for the fuse permission too
+        if (!modelType.equals("a") &&
+                !modelType.equals("b") &&
+                !modelType.equals("c") &&
+                !modelType.equals("d") &&
+                !modelType.equals("f") &&
+                !player.hasFuse("fuse_use_special_room_layouts")) {
+            return; // Fuck off, scripter.
+        }
 
         int accessType = 0;
 
