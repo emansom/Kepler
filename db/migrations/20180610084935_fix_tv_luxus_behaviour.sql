32cf6a4b3128b25f1de715266cde531848d5334a
Quackster <quacksterr.15@gmail.com>
2018-06-10 18:53:02 +1000

    Use sprite instead of ID

--- db/migrations/20180610084935_fix_tv_luxus_behaviour.sql
+++ db/migrations/20180610084935_fix_tv_luxus_behaviour.sql
@@ -1,5 +1,5 @@
 -- migrate:up
-UPDATE `items_definitions` SET `behaviour` = 'solid,custom_data_on_off' WHERE `id` = 83;
+UPDATE `items_definitions` SET `behaviour` = 'solid,custom_data_on_off' WHERE `sprite` = 'tv_luxus';
 
 -- migrate:down
 
