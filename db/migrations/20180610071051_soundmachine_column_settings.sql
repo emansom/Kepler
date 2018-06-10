9199f1da3299bb534cf040de29ef89be05fbfe17
Quackster <quacksterr.15@gmail.com>
2018-06-10 17:15:55 +1000

    Remove COMMIT;

--- db/migrations/20180610071051_soundmachine_column_settings.sql
+++ db/migrations/20180610071051_soundmachine_column_settings.sql
@@ -7,7 +7,7 @@ ALTER TABLE `soundmachine_playlists`
   ADD KEY `songid` (`song_id`);
   
 ALTER TABLE `soundmachine_songs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 -- migrate:down
 
