88a6964a2c1c0d362b5c817b250cf91ba6e10203
Quackster <quacksterr.15@gmail.com>
2018-07-01 16:09:24 +1000

    Add alert when rainbow task has started

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/registered/RainbowDimmerCommand.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/registered/RainbowDimmerCommand.java
@@ -65,6 +65,7 @@ public class RainbowDimmerCommand extends Command {
         } else {
             RainbowTask rainbowTask = new RainbowTask(room);
             room.getTaskManager().scheduleTask("RainbowTask", rainbowTask, tickInterval, TimeUnit.SECONDS);
+            player.send(new CHAT_MESSAGE(CHAT_MESSAGE.type.WHISPER, player.getRoomUser().getInstanceId(), "Rainbow room dimmer cycle has started"));
         }
     }
 
