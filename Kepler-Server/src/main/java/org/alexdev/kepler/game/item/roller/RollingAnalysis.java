a3ec105bf708940d2a5770ff9133768062c663fd
Alex <quacksterr.15@gmail.com>
2018-07-05 15:04:11 +1000

    Use T instead to show it's a generic type

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/item/roller/RollingAnalysis.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/item/roller/RollingAnalysis.java
@@ -3,7 +3,7 @@ package org.alexdev.kepler.game.item.roller;
 import org.alexdev.kepler.game.item.Item;
 import org.alexdev.kepler.game.pathfinder.Position;
 
-public interface RollingAnalysis<RollingType> {
-    public Position canRoll(RollingType rollingType, Item roller);
-    public void doRoll(RollingType rollingType, Position nextPosition);
+public interface RollingAnalysis<T> {
+    public Position canRoll(T rollingType, Item roller);
+    public void doRoll(T rollingType, Position nextPosition);
 }
