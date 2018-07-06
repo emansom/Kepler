0936822bef7b86263fbaf2ec62dc6234d76bcc0f
Alex <quacksterr.15@gmail.com>
2018-07-06 12:17:22 +1000

    Initialise Kepler on unit testing

--- Kepler-Server/src/test/java/RollerTest.java
+++ Kepler-Server/src/test/java/RollerTest.java
@@ -1,7 +1,12 @@
 import junit.framework.TestCase;
+import org.alexdev.kepler.Kepler;
 import org.junit.Assert;
 
 public class RollerTest extends TestCase {
+    public RollerTest() {
+        Kepler.main(null);
+    }
+
     public void testEntityRoll() {
 
     }
