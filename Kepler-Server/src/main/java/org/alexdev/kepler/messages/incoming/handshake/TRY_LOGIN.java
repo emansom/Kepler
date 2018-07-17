d2069e11ae59c7cef152b2f054a09a5110c4c678
Quackster <quacksterr.15@gmail.com>
2018-07-17 18:47:10 +1000

    Filter username/password input for when using TAB

--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/handshake/TRY_LOGIN.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/handshake/TRY_LOGIN.java
@@ -5,13 +5,14 @@ import org.alexdev.kepler.game.player.Player;
 import org.alexdev.kepler.messages.outgoing.user.LOCALISED_ERROR;
 import org.alexdev.kepler.messages.types.MessageEvent;
 import org.alexdev.kepler.server.netty.streams.NettyRequest;
+import org.alexdev.kepler.util.StringUtil;
 
 public class TRY_LOGIN implements MessageEvent {
 
     @Override
     public void handle(Player player, NettyRequest reader) {
-        String username = reader.readString();
-        String password = reader.readString();
+        String username = StringUtil.filterInput(reader.readString(), true);
+        String password = StringUtil.filterInput(reader.readString(), true);
 
         if (!PlayerDao.login(player, username, password)) {
             player.send(new LOCALISED_ERROR("Login incorrect"));
