6320539e0a8319d689a4e7f8db3a48988951c6e8
Alex <quacksterr.15@gmail.com>
2018-07-18 08:07:05 +1000

    Temporarily disable channelReadComplete override

--- Kepler-Server/src/main/java/org/alexdev/kepler/server/netty/connections/ConnectionHandler.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/server/netty/connections/ConnectionHandler.java
@@ -70,20 +70,20 @@ public class ConnectionHandler extends SimpleChannelInboundHandler<NettyRequest>
         }
     }
 
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        try {
-            Player player = ctx.channel().attr(Player.PLAYER_KEY).get();
-
-            if (player.isDisconnected()) {
-                return;
-            }
-
-            player.getNetwork().flush();
-        } catch (Exception ex) {
-            Log.getErrorLogger().error("Exception occurred handling channelReadComplete: ", ex);
-        }
-    }
+//    @Override
+//    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+//        try {
+//            Player player = ctx.channel().attr(Player.PLAYER_KEY).get();
+//
+//            if (player.isDisconnected()) {
+//                return;
+//            }
+//
+//            player.getNetwork().flush();
+//        } catch (Exception ex) {
+//            Log.getErrorLogger().error("Exception occurred handling channelReadComplete: ", ex);
+//        }
+//    }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
