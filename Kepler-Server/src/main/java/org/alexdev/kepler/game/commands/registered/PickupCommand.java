f235a577f94bbd30821aae17d3480134d6c1986f
Quackster <quacksterr.15@gmail.com>
2018-07-19 02:16:36 +1000

    Rename variables to correct and descriptive names

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/registered/PickupCommand.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/registered/PickupCommand.java
@@ -35,7 +35,7 @@ public class PickupCommand extends Command {
             return;
         }
 
-        List<Item> itemsToDelete = new ArrayList<>();
+        List<Item> itemsToAdd = new ArrayList<>();
         List<Item> itemsToUpdate = new ArrayList<>();
 
         for (Item item : player.getRoomUser().getRoom().getItems()) {
@@ -54,11 +54,11 @@ public class PickupCommand extends Command {
                 ItemDao.deleteItem(item.getId());
             } else {
                 player.getInventory().getItems().add(item);
-                itemsToDelete.add(item);
+                itemsToAdd.add(item);
             }
         }
 
-        ItemDao.updateItems(itemsToDelete);
+        ItemDao.updateItems(itemsToAdd);
 
         player.getInventory().getView("last");
         player.send(new ALERT("All furniture items have been picked up"));
