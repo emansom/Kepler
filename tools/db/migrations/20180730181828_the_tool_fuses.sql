40b0d213eb4672b66b393d02f155afafe2bf49bf
Raptosaur <Raptosaur@users.noreply.github.com>
2018-07-30 19:21:47 +0100

    Added Mod Tool fuses

---
+++ tools/db/migrations/20180730181828_the_tool_fuses.sql
@@ -0,0 +1,8 @@
+-- migrate:up
+INSERT INTO `rank_fuserights` VALUES (4, 'fuse_alert');
+INSERT INTO `rank_fuserights` VALUES (4, 'fuse_room_alert');
+INSERT INTO `rank_fuserights` VALUES (4, 'fuse_room_kick');
+INSERT INTO `rank_fuserights` VALUES (4, 'fuse_kick');
+
+-- migrate:down
+
