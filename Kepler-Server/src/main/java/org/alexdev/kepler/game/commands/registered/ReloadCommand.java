f295db67b5d895454466c261301ef5bd78a92811
Alex <quacksterr.15@gmail.com>
2018-08-02 08:51:57 +1000

    Add reload models

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/registered/ReloadCommand.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/registered/ReloadCommand.java
@@ -6,6 +6,7 @@ import org.alexdev.kepler.game.entity.Entity;
 import org.alexdev.kepler.game.entity.EntityType;
 import org.alexdev.kepler.game.item.ItemManager;
 import org.alexdev.kepler.game.player.Player;
+import org.alexdev.kepler.game.room.models.RoomModelManager;
 import org.alexdev.kepler.game.texts.TextsManager;
 import org.alexdev.kepler.messages.outgoing.rooms.user.CHAT_MESSAGE;
 import org.alexdev.kepler.messages.outgoing.user.ALERT;
@@ -54,6 +55,11 @@ public class ReloadCommand extends Command {
             componentName = "Texts";
         }
 
+        if (component.equalsIgnoreCase("models")) {
+            RoomModelManager.reset();
+            componentName = "Room models";
+        }
+
         if (component.equalsIgnoreCase("settings")) {
             GameConfiguration.reset();
             componentName = "Game settings";
@@ -62,7 +68,7 @@ public class ReloadCommand extends Command {
         if (componentName != null) {
             player.send(new ALERT(componentName + " has been reloaded."));
         } else {
-            player.send(new ALERT("You did not specify which component to reload! You may reload either the catalogue/shop/items, texts or game settings."));
+            player.send(new ALERT("You did not specify which component to reload!<br>You may reload either the catalogue/shop/items, models, texts or game settings."));
         }
     }
 
