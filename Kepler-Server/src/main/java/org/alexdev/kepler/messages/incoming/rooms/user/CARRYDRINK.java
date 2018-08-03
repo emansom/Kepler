1fdc9780d6e77c80d0ceb89c0bdfdc11a8b3e567
Alex <quacksterr.15@gmail.com>
2018-08-04 07:55:58 +1000

    Don't allow CARRYDRINk if they're carrying an item

--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/rooms/user/CARRYDRINK.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/rooms/user/CARRYDRINK.java
@@ -1,6 +1,7 @@
 package org.alexdev.kepler.messages.incoming.rooms.user;
 
 import org.alexdev.kepler.game.player.Player;
+import org.alexdev.kepler.game.room.enums.StatusType;
 import org.alexdev.kepler.messages.types.MessageEvent;
 import org.alexdev.kepler.server.netty.streams.NettyRequest;
 import org.alexdev.kepler.util.StringUtil;
@@ -9,6 +10,10 @@ public class CARRYDRINK implements MessageEvent {
     @Override
     public void handle(Player player, NettyRequest reader) {
         if (player.getRoomUser().getRoom() == null) {
+            return;
+        }
+
+        if (player.getRoomUser().containsStatus(StatusType.CARRY_ITEM)) {
             return;
         }
 
