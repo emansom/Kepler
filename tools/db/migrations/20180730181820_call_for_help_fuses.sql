8d0838224083721bcd32a4c238c912e49336bcd1
Alex <quacksterr.15@gmail.com>
2018-08-10 10:52:33 +1000

    Remove unneeded migrations

--- tools/db/migrations/20180730181820_call_for_help_fuses.sql
+++
@@ -1,5 +0,0 @@
--- migrate:up
-INSERT INTO `rank_fuserights` VALUES (4, 'fuse_receive_calls_for_help');
-
--- migrate:down
-
--- tools/db/migrations/20180730181828_the_tool_fuses.sql
+++
@@ -1,8 +0,0 @@
--- migrate:up
-INSERT INTO `rank_fuserights` VALUES (4, 'fuse_alert');
-INSERT INTO `rank_fuserights` VALUES (4, 'fuse_room_alert');
-INSERT INTO `rank_fuserights` VALUES (4, 'fuse_room_kick');
-INSERT INTO `rank_fuserights` VALUES (4, 'fuse_kick');
-
--- migrate:down
-
