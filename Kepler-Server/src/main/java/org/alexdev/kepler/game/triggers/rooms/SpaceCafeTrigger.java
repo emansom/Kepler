7849f404f41ee440ddf7fad1009581f4e84e9d02
Quackster <quacksterr.15@gmail.com>
2018-08-16 21:11:37 +1000

    Fix compile error

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/triggers/rooms/SpaceCafeTrigger.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/triggers/rooms/SpaceCafeTrigger.java
@@ -1,17 +1,19 @@
 package org.alexdev.kepler.game.triggers.rooms;
 
 import org.alexdev.kepler.game.entity.Entity;
+import org.alexdev.kepler.game.player.Player;
+import org.alexdev.kepler.game.room.Room;
 import org.alexdev.kepler.game.room.entities.RoomEntity;
 import org.alexdev.kepler.game.triggers.GenericTrigger;
 
 public class SpaceCafeTrigger extends GenericTrigger {
     @Override
-    public void onRoomEntry(Entity entity, RoomEntity roomEntity, Object... customArgs) {
+    public void onRoomEntry(Player player, Room room, Object... customArgs) {
 
     }
 
     @Override
-    public void onRoomLeave(Entity entity, RoomEntity roomEntity, Object... customArgs) {
+    public void onRoomLeave(Player player, Room room, Object... customArgs) {
 
     }
 }
