243cfe373bee640cbd47086a1a23eceb38a2eb14
Alex <quacksterr.15@gmail.com>
2018-08-17 13:37:00 +1000

    Remove unneeded "rooms" from class path

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/room/models/RoomModel.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/room/models/RoomModel.java
@@ -32,7 +32,7 @@ public class RoomModel {
 
         if (!StringUtil.isNullOrEmpty(triggerClass)) {
             try {
-                Class<?> clazz = Class.forName("org.alexdev.kepler.game.room.triggers.rooms." + triggerClass);
+                Class<?> clazz = Class.forName("org.alexdev.kepler.game.room.triggers." + triggerClass);
                 this.modelTrigger = (GenericTrigger) clazz.getDeclaredConstructor().newInstance();
             } catch (Exception e) {
                 e.printStackTrace();
