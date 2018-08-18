90e555342880a447cc9db8fef3e222d6b746185e
Alex <quacksterr.15@gmail.com>
2018-08-19 06:33:03 +1000

    Add :setconfig

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/CommandManager.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/CommandManager.java
@@ -44,6 +44,7 @@ public class CommandManager {
         this.commands.put(new String[] { "reload" }, new ReloadCommand());
         this.commands.put(new String[] { "shutdown" }, new ShutdownCommand());
         this.commands.put(new String[] { "setprice" }, new SetItemPriceCommand());
+        this.commands.put(new String[] { "setconfig" }, new SetConfigCommand());
 
         // Add client-side commands to list
         this.commands.put(new String[] { "chooser" }, new ChooserCommand());
