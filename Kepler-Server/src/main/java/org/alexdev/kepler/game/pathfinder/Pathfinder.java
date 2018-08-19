24a7060598ff96ce015e5199b4098b05f09edf8b
Quackster <quacksterr.15@gmail.com>
2018-08-20 00:41:57 +1000

    Fix pool entry/exit triggering glitches when spamming enter/exit

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/pathfinder/Pathfinder.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/pathfinder/Pathfinder.java
@@ -61,14 +61,14 @@ public class Pathfinder {
 
         if (oldHeight - 3 >= newHeight) {
             return fromItem != null && (fromItem.hasBehaviour(ItemBehaviour.TELEPORTER)
-                    || fromItem.getDefinition().getSprite().equals("poolEnter")
-                    || fromItem.getDefinition().getSprite().equals("poolExit"));
+                    || (fromItem.getDefinition().getSprite().equals("poolEnter" )&& !entity.getRoomUser().containsStatus(StatusType.SWIM)) // Allow height difference only if they're heading to exit and swimming
+                    || (fromItem.getDefinition().getSprite().equals("poolExit") && entity.getRoomUser().containsStatus(StatusType.SWIM))); // Allow height difference only if they're heading to entry and  not swimming
         }
 
         if (oldHeight + 1.5 <= newHeight) {
             return toItem != null && (toItem.hasBehaviour(ItemBehaviour.TELEPORTER)
-                    || toItem.getDefinition().getSprite().equals("poolEnter")
-                    || toItem.getDefinition().getSprite().equals("poolExit"));
+                    || (toItem.getDefinition().getSprite().equals("poolEnter" )&& !entity.getRoomUser().containsStatus(StatusType.SWIM)) // Allow height difference only if they're heading to exit and swimming
+                    || (toItem.getDefinition().getSprite().equals("poolExit") && entity.getRoomUser().containsStatus(StatusType.SWIM))); // Allow height difference only if they're heading to entry and  not swimming
         }
 
         // Only check these below if the user is in a pool room.
