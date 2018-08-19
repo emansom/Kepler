d2283ccf6cdedc9f9e80a929509518ba0a4e5ee7
Alex <quacksterr.15@gmail.com>
2018-08-19 11:02:04 +1000

    Select previous tile item when rolling

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/room/entities/RoomEntity.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/room/entities/RoomEntity.java
@@ -200,7 +200,7 @@ public abstract class RoomEntity {
             this.position.setZ(height);
         }
 
-        Item item = this.getCurrentItem();
+        Item item = isRolling ? this.room.getMapping().getTile(this.rollingData.getFromPosition()).getHighestItem() : this.getCurrentItem();
 
         if (item == null || (!item.hasBehaviour(ItemBehaviour.CAN_SIT_ON_TOP) || !item.hasBehaviour(ItemBehaviour.CAN_LAY_ON_TOP))) {
             if (this.containsStatus(StatusType.SIT) || this.containsStatus(StatusType.LAY)) {
