ac84d1dbce1c2d1381ea6585a865e93853b72ae5
Alex <quacksterr.15@gmail.com>
2018-08-22 13:08:06 +1000

    Add source comment explanation

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/item/Item.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/item/Item.java
@@ -154,7 +154,10 @@ public class Item {
 
     /**
      * Gets if the teleporter can be moved, if the pair is broken then the teleporter
-     * is also considered moveable.
+     * is also considered moveable, only checks if the teleporters are across different rooms.
+     * 
+     * This is because if you try to pick it up while using a teleporter across rooms, the client will refuse to send another
+     * teleport request and you'll be unable to use teleporters until you relog.
      *
      * @return if it is moveable
      */
