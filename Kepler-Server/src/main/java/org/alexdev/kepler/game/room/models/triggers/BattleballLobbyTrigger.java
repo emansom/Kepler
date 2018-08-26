09c2bf3031be77bb6c0702dbcf6ac7995a517488
Alex <quacksterr.15@gmail.com>
2018-08-26 12:57:05 +1000

    Fix showing score levels on room entry

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/room/models/triggers/BattleballLobbyTrigger.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/room/models/triggers/BattleballLobbyTrigger.java
@@ -14,6 +14,7 @@ import org.alexdev.kepler.messages.outgoing.games.LOUNGEINFO;
 import org.alexdev.kepler.messages.outgoing.games.GAMEPLAYERINFO;
 import org.alexdev.kepler.util.StringUtil;
 
+import java.util.List;
 import java.util.Map;
 
 public class BattleballLobbyTrigger extends GameLobbyTrigger {
@@ -26,7 +27,12 @@ public class BattleballLobbyTrigger extends GameLobbyTrigger {
         Player player = (Player) entity;
 
         player.send(new LOUNGEINFO());
+
+        // Show you everyones score level
         player.send(new GAMEPLAYERINFO(this.getGameType(), room.getEntityManager().getPlayers()));
+
+        // Show everyone your score level
+        room.send(new GAMEPLAYERINFO(this.getGameType(), List.of(player)));
     }
 
     @Override
