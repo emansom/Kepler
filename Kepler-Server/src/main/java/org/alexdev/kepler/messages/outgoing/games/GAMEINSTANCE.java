a3aac19613b9da4976794339458a6f31fdef7746
Quackster <quacksterr.15@gmail.com>
2018-08-30 02:54:53 +1000

    Fix structure

--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/outgoing/games/GAMEINSTANCE.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/outgoing/games/GAMEINSTANCE.java
@@ -1,6 +1,7 @@
 package org.alexdev.kepler.messages.outgoing.games;
 
 import org.alexdev.kepler.game.games.Game;
+import org.alexdev.kepler.game.games.GameState;
 import org.alexdev.kepler.game.games.player.GamePlayer;
 import org.alexdev.kepler.messages.types.MessageComposer;
 import org.alexdev.kepler.server.netty.streams.NettyResponse;
@@ -17,37 +18,59 @@ public class GAMEINSTANCE extends MessageComposer {
     @Override
     public void compose(NettyResponse response) {
         response.writeInt(this.game.getGameState().getStateId());
-        response.writeBool(this.game.canGameStart());
-        response.writeString(this.game.getName());
-        response.writeInt(this.game.getGameCreator().getRoomUser().getInstanceId());
-        response.writeString(this.game.getGameCreator().getDetails().getName());
 
-        // TODO: Special SnowStorm parameters
+        if (this.game.getGameState() == GameState.WAITING) {
+            response.writeInt(this.game.getId());
+            response.writeString(this.game.getName());
 
-        response.writeInt(this.game.getMapId());
-        response.writeInt(this.game.getSpectators().size());
-        response.writeInt(this.game.getTeamAmount());
+            response.writeInt(this.game.getGameCreator().getRoomUser().getInstanceId());
+            response.writeString(this.game.getGameCreator().getDetails().getName());
 
-        for (int i = 0; i < this.game.getTeamAmount(); i++) {
-            List<GamePlayer> playerList = this.game.getTeams().get(i).getPlayers();
+            response.writeInt(this.game.getMapId());
 
-            response.writeInt(playerList.size());
+            response.writeInt(this.game.getSpectators().size());
+            response.writeInt(this.game.getTeamAmount());
 
-            for (GamePlayer player : playerList) {
-                response.writeInt(player.getPlayer().getRoomUser().getInstanceId());
-                response.writeString(player.getPlayer().getDetails().getName());
+            for (int i = 0; i < this.game.getTeamAmount(); i++) {
+                List<GamePlayer> playerList = this.game.getTeams().get(i).getPlayers();
+
+                response.writeInt(playerList.size());
+
+                for (GamePlayer player : playerList) {
+                    response.writeInt(player.getPlayer().getRoomUser().getInstanceId());
+                    response.writeString(player.getPlayer().getDetails().getName());
+                }
             }
         }
 
-        // TODO: Special SnowStorm parameters
+        if (this.game.getGameState() == GameState.STARTED) {
+            response.writeInt(this.game.getId());
+            response.writeString(this.game.getName());
+            response.writeString(this.game.getGameCreator().getDetails().getName());
+            response.writeInt(this.game.getMapId());
+            response.writeInt(this.game.getTeamAmount());
 
-        String[] powerUps = new String[this.game.getPowerUps().size()];
+            for (int i = 0; i < this.game.getTeamAmount(); i++) {
+                List<GamePlayer> playerList = this.game.getTeams().get(i).getPlayers();
 
-        for (int i = 0; i < this.game.getPowerUps().size(); i++) {
-            powerUps[i] = String.valueOf(this.game.getPowerUps().get(i));
+                response.writeInt(playerList.size());
+
+                for (GamePlayer player : playerList) {
+                    //response.writeInt(player.getPlayer().getRoomUser().getInstanceId());
+                    response.writeString(player.getPlayer().getDetails().getName());
+                }
+            }
+
+            // TODO: Special SnowStorm parameters
+
+            String[] powerUps = new String[this.game.getPowerUps().size()];
+
+            for (int i = 0; i < this.game.getPowerUps().size(); i++) {
+                powerUps[i] = String.valueOf(this.game.getPowerUps().get(i));
+            }
+
+            response.writeString(String.join(",", powerUps));
         }
-
-        response.writeString(String.join(",", powerUps));
     }
 
     @Override
