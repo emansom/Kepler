21e2dd5f6c89bfd4f1877026443eddfec7be210e
Alex <quacksterr.15@gmail.com>
2018-08-30 14:11:36 +1000

    Revert time test

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/games/Game.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/games/Game.java
@@ -52,8 +52,8 @@ public class Game {
     private AtomicLong restartCountdown;
 
     public static final int PREPARING_GAME_SECONDS_LEFT = 10;
-    public static final int RESTART_GAME_SECONDS = 30;
-    public static final int GAME_LENGTH_SECONDS =  180;
+    public static final int RESTART_GAME_SECONDS = 10;
+    public static final int GAME_LENGTH_SECONDS =  10;
 
     private FutureRunnable preparingTimerRunnable;
     private FutureRunnable gameTimerRunnable;
