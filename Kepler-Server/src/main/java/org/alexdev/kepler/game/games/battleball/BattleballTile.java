a285b7acbcd342c21b322f0b002e68fc340057e4
Alex <quacksterr.15@gmail.com>
2018-09-09 20:38:47 +1000

    Allow fill algorithim for one square and higher

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/games/battleball/BattleballTile.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/games/battleball/BattleballTile.java
@@ -301,7 +301,7 @@ public class BattleballTile extends GameTile  {
 
             var fillTiles = FloodFill.getFill(gamePlayer, neighbour);
 
-            if (fillTiles.size() > 1) {
+            if (fillTiles.size() > 0) {
                 for (BattleballTile filledTile : FloodFill.getFill(gamePlayer, neighbour)) {
                     if (filledTile.getState() == BattleballTileType.SEALED) {
                         continue;
