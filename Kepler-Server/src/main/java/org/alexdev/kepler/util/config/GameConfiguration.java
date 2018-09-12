0a69c6ca6edbaf62718740845173da2f97f8e7c2
Quackster <quacksterr.15@gmail.com>
2018-09-13 05:13:20 +1000

    Set increase points to true by default

--- Kepler-Server/src/main/java/org/alexdev/kepler/util/config/GameConfiguration.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/util/config/GameConfiguration.java
@@ -58,7 +58,7 @@ public class GameConfiguration {
         config.put("battleball.game.lifetime.seconds", "180");
         config.put("battleball.restart.game.seconds", "30");
         config.put("battleball.ticket.charge", "2");
-        config.put("battleball.increase.points", "false");
+        config.put("battleball.increase.points", "true");
 
         config.put("game.finished.listing.expiry.seconds", "300");
 
@@ -68,7 +68,7 @@ public class GameConfiguration {
         //config.put("snowstorm.game.lifetime.seconds", "180");
         config.put("snowstorm.restart.game.seconds", "30");
         config.put("snowstorm.ticket.charge", "2");
-        config.put("snowstorm.increase.points", "false");
+        config.put("snowstorm.increase.points", "true");
 
         config.put("tutorial.enabled", "true");
         config.put("profile.editing", "true");
