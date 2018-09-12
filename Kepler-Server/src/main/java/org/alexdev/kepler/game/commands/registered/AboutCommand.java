73f747d921927aa4ac287270bd2a75fd15df58a4
Alex <quacksterr.15@gmail.com>
2018-09-12 15:32:44 +1000

    Don't use String.join like before

--- Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/registered/AboutCommand.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/game/commands/registered/AboutCommand.java
@@ -22,19 +22,14 @@ public class AboutCommand extends Command {
 
         Player player = (Player)entity;
         
-        player.send(new ALERT("Project Kepler<br><br>Contributors:" +
-                "<br> - Hoshiko" + // Aside from myself (Quackster), Hoshiko contributed a lot.
-                "<br> - ThuGie" + // Helping me with knowing the Habbo protocol better
-                "<br> - Alito" + // Bug tester and contributor for rooms/fixed badges
-                "<br> - Ascii" + // Helping me with various packet structures
-                "<br> - Lightbulb" + // Helping me with getting tables to show up in public rooms
-                "<br> - Raptosaur " + // Call for help
-                "<br> - Romuald" + // Creating Habbo Club subscription
-                "<br> - Glaceon" + // Creating redeeming furni
-                "<br> - Nillus/Holo Team" + // Various parts of Woodpecker, Holograph assisted me, A LOT with protocol
-                "<br> - Meth0d" + // Uber had the Club Mammoth furni data
-                "<br> - office.boy" + // Parts of Blunk
-                "<br><br>Made by Quackster from RaGEZONE"));
+        player.send(new ALERT("Project Kepler - Habbo Hotel v21 emulation" +
+                "<br>" +
+                "<br>Contributors:" +
+                "<br> - Hoshiko, ThuGie, Alito, Ascii, Lightbulb, Raptosaur " + // Call for help
+                "<br>   Romuald, Glaceon, Nillus, Holo Team, Meth0d, office.boy" + // Parts of Blunk
+                "<br>" +
+                "<br>" +
+                "Made by Quackster from RaGEZONE"));
     }
 
     @Override
